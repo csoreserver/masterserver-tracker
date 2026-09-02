8847ac64dd96251ada79ea64aab517d122a22616
Lieutenant Eva Lee <156443123+LieutenantEva@users.noreply.github.com>
2026-09-02 19:30:52 +0700

    main.py: Rework health check mechanism, New server status, Reduce datastore watcher's reload call

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import enum
+import threading
 import socket
 import time
 import pathlib
 import json
 from os import (
     environ,
+)
+from io import (
+    DEFAULT_BUFFER_SIZE,
 )
 import dotenv
 import requests
@@ -32,10 +37,14 @@ def load_datastore ():
         DATASTORE = json.load(fp)
 
 def save_datastore ():
-    global DATASTORE
-    ds = pathlib.Path(DATASTORE_PATH)
-    with ds.open("r+") as fp:
-        json.dump(DATASTORE, fp)
+    global DATASTORE, FILEWATCHER
+    FILEWATCHER.unschedule_all()
+    try:
+        ds = pathlib.Path(DATASTORE_PATH)
+        with ds.open("r+") as fp:
+            json.dump(DATASTORE, fp)
+    finally:
+        FILEWATCHER.schedule(ModifiedHandler(), "./", recursive=False)
 
 
 CONFIG_PATH = ".env"
@@ -45,69 +54,93 @@ def load_config ():
     DATASTORE_PATH = environ["CSORSE_MASTERSERVER_DATASTORE_PATH"]
 
 
-def check_server () -> dict:
-    timeout = int(environ["CSORSE_MASTERSERVER_TIMEOUT"])
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:
-            s.settimeout(timeout)
-            s.connect((
-                environ["CSORSE_MASTERSERVER_IP"],
-                int(environ["CSORSE_MASTERSERVER_PORT"])
-            ))
-            s.settimeout(timeout)
-            data = s.recv(1024)
-            if data not in [b"~SERVERCONNECTED\n", b"~SERVERCONNECTED\n\x00"]:
-                raise RuntimeError("Not a master server instance")
-        except (ConnectionRefusedError, TimeoutError):
-            return  {
-                "success": True,
-                "online": False,
-            }
-        except Exception as error:
-            return {
-                "success": False,
-                "error": f"{type(error).__name__}: {error}"
-            }
-    return  {
-        "success": True,
-        "online": True,
+class ModifiedHandler (FileSystemEventHandler):
+    def on_modified(self, event: FileModifiedEvent):
+        if type(event) is not FileModifiedEvent:
+            return
+        src_path = pathlib.Path(event.src_path).resolve(False)
+        if src_path == pathlib.Path(CONFIG_PATH).resolve(False):
+            print(f"{CONFIG_PATH} has been modified. Reload now...")
+            load_config()
+        elif src_path == pathlib.Path(DATASTORE_PATH).resolve(False):
+            print(f"{DATASTORE_PATH} has been modified. Reload now...")
+            load_datastore()
+
+FILEWATCHER: Observer = None
+def filewatch_init ():
+    load_config()
+    load_datastore()
+
+    filewatcher = Observer()
+    filewatcher.schedule(ModifiedHandler(), "./", recursive=False)
+    filewatcher.start()
+    return filewatcher
+
+
+class Status (enum.Enum):
+    UNKNOWN = enum.auto()
+    CONNECTING = enum.auto()
+    CONNECTED = enum.auto()
+    DISCONNECTED = enum.auto()
+    IDLE = enum.auto()
+    ERROR = enum.auto()
+    EXIT = enum.auto()
+
+
+CONN_FMT = "<t:{timestamp}:S>"
+def update_discord (status: Status = Status.UNKNOWN, error: str = ""):
+    global DATASTORE
+    print(f"Send status: {status.name}")
+    ds_embed = {
+        "title": "CSO Re-Server Master Server Tracker",
+        "fields": [
+            {
+                "name": "Last Time Connected",
+                "value": "None",
+                "inline": False
+            },
+            {
+                "name": "Status",
+                "value": "❓Unknown",
+                "inline": False
+            },
+        ],
     }
+    ds_fields = ds_embed["fields"]
+    ds_field_status = ds_fields[1]
 
+    ts_last: int = int(DATASTORE.get("ms_conn_last_time", 0))
 
-def update_discord (check_result):
-    ts_cur = int(time.time() // 1)
-    ts_next = ts_cur + int(environ["CSORSE_MASTERSERVER_UPDATE_INTERVAL"])
+    match status:
+        case Status.IDLE:
+            ds_field_status["value"] = "🟡Idle"
+        case Status.CONNECTING:
+            ds_field_status["value"] = "🔄Connecting"
+        case Status.CONNECTED:
+            ds_field_status["value"] = "🟢Connected"
+            ts_last = int(time.time() // 1)
+        case Status.DISCONNECTED:
+            ds_field_status["value"] = "🔴Disconnected"
+        case Status.ERROR:
+            ds_field_status["value"] = f"⁉️Error\n{error}"
+        case Status.EXIT:
+            ds_field_status["value"] = f"💤Sleep"
+        case _:
+            ds_field_status["value"] = "❓Unknown"
 
-    status = "🔴Offline"
-    if check_result["success"]:
-        status = "🟢Online" if check_result["online"] else "🔴Offline"
-    else:
-        status = f"⁉️Error\n{check_result['error']}"
+    if ts_last:
+        ds_fields[0]["value"] = CONN_FMT.format(timestamp=ts_last)
+        DATASTORE["ms_conn_last_time"] = ts_last
 
-    data=json.dumps({
-        "embeds": [
-        {
-            "title": "CSO Re-Server Master Server Tracker",
-            "fields": [
-                {
-                    "name": "Last Time Checked",
-                    "value": f"<t:{ts_cur}:S>",
-                    "inline": False
-                },
-                {
-                    "name": "Status",
-                    "value": status,
-                    "inline": False
-                },
-                {
-                    "name": "Next Check Time",
-                    "value": f"<t:{ts_next}:R>",
-                    "inline": False
-                }
-            ]
-        }
-        ]
-    })
+    if status not in [Status.CONNECTING, Status.CONNECTED, Status.EXIT]:
+        ts_next = int(time.time() // 1) + int(environ["CSORSE_MASTERSERVER_UPDATE_INTERVAL"])
+        ds_fields.append({
+            "name": "Next Retry Time",
+            "value": f"<t:{ts_next}:R>",
+            "inline": False
+        })
+
+    data=json.dumps({"embeds": [ds_embed]})
     headers = {
         "Content-Type": "application/json",
     }
@@ -140,39 +173,73 @@ def update_discord (check_result):
     return r.json()
 
 
+class SockRecvForever (threading.Thread):
+    def __init__ (self, s: socket.socket, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sock = s
+        self.exc: BaseException = None
+
+    def run (self):
+        s = self.sock
+        while s:
+            try:
+                s.settimeout(None)
+                s.recv(DEFAULT_BUFFER_SIZE)
+            except (BaseException,) as exc:
+                self.exc = exc
+                break
+
+
 def main ():
-    load_config()
-    load_datastore()
+    global DATASTORE, FILEWATCHER
+    FILEWATCHER = filewatch_init()
 
-    class ModifiedHandler (FileSystemEventHandler):
-        def on_modified(self, event: FileModifiedEvent):
-            if type(event) is not FileModifiedEvent:
-                return
-            src_path = pathlib.Path(event.src_path).resolve(False)
-            if src_path == pathlib.Path(CONFIG_PATH).resolve(False):
-                print(f"{CONFIG_PATH} has been modified. Reload now...")
-                load_config()
-            elif src_path == pathlib.Path(DATASTORE_PATH).resolve(False):
-                print(f"{DATASTORE_PATH} has been modified. Reload now...")
-                load_datastore()
-
-    filewatcher = Observer()
-    filewatcher.schedule(ModifiedHandler(), "./", recursive=False)
-    filewatcher.start()
-
-    while True:
-        print(f"Checking server status: {environ['CSORSE_MASTERSERVER_IP']} {environ['CSORSE_MASTERSERVER_PORT']}")
-        status = check_server()
-        print(f"Response: {status}")
-        print(f"Send Discord Message: {'New' if 'ms_discord_msg_id' not in DATASTORE else DATASTORE['ms_discord_msg_id']}")
-        response = update_discord(status)
-        print(f"Response: {response}")
+    def send_status_and_store (*args, **kwargs):
+        response = update_discord(*args, **kwargs)
         DATASTORE["ms_discord_msg_id"] = response["id"]
         save_datastore()
-        interval = float(environ["CSORSE_MASTERSERVER_UPDATE_INTERVAL"])
-        print(f"Sleep for {interval} second(s)")
-        print(f"==============================")
-        time.sleep(interval)
+
+    send_status_and_store()
+
+    timeout = int(environ["CSORSE_MASTERSERVER_TIMEOUT"])
+    sock_thread: SockRecvForever = None
+
+    try:
+        while True:
+            send_status_and_store(Status.IDLE)
+            print(f"Checking server status: {environ['CSORSE_MASTERSERVER_IP']} {environ['CSORSE_MASTERSERVER_PORT']}")
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                try:
+                    send_status_and_store(Status.CONNECTING)
+                    s.settimeout(timeout)
+                    s.connect((
+                        environ["CSORSE_MASTERSERVER_IP"],
+                        int(environ["CSORSE_MASTERSERVER_PORT"])
+                    ))
+                    s.settimeout(timeout)
+                    data = s.recv(1024)
+                    if data not in [b"~SERVERCONNECTED\n", b"~SERVERCONNECTED\n\x00"]:
+                        raise RuntimeError("Not a master server instance")
+                    send_status_and_store(Status.CONNECTED)
+                    if not sock_thread:
+                        sock_thread = SockRecvForever(s, daemon=True)
+                        sock_thread.start()
+                    sock_thread.join()
+                    if sock_thread.exc:
+                        raise sock_thread.exc
+                except (ConnectionRefusedError, TimeoutError, ConnectionResetError):
+                    send_status_and_store(Status.DISCONNECTED)
+                except Exception as error:
+                    send_status_and_store(Status.ERROR, f"{type(error).__name__}: {error}")
+                finally:
+                    sock_thread = None
+            save_datastore()
+            interval = float(environ["CSORSE_MASTERSERVER_UPDATE_INTERVAL"])
+            print(f"Sleep for {interval} second(s)")
+            print(f"==============================")
+            time.sleep(interval)
+    finally:
+        send_status_and_store(Status.EXIT)
 
 if __name__ == "__main__":
     main()
